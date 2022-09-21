7150ae6c123e0f062b15b8653d5ccfc805a66363
elis-11 <eliza.arzanukaeva@gmail.com>
2022-09-21 19:17:07 +0200

    TS - Create ProductList - Component

--- ts-store/src/components/admin/ProductList.tsx
+++ ts-store/src/components/admin/ProductList.tsx
@@ -1,7 +1,81 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useDataContext } from "../../context/DataProvider";
+import { deleteProductApi } from "../../helpers/ApiCalls";
+import { IProduct } from "../../types/product.types";
 
-export const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
+type Props = {
+    products: IProduct[]
 }
+
+export const ProductList = ({products}: Props) => {
+  const { user,  setProducts, errors, setErrors } = useDataContext();
+
+  //! edit
+
+  //! delete
+  const handleDelete = async (productId: string) => {
+    if (!user) return;
+
+    // 1.step => delete product at API
+    const response = await deleteProductApi(user.token, productId);
+
+    // 2.step => delete product in state
+    const productsCopy = products.filter(
+      (product) => product._id !== productId
+    );
+    console.log(productsCopy);
+
+    setProducts(productsCopy);
+  };
+
+  return (
+    <div>
+      {user && (
+        <div className="content">
+          {([...products] || []).reverse().map((product) => (
+            <div key={product._id} className="product">
+              <div>
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className="data">
+                <div className="name">{product.name}</div>
+                <div>{product.description}</div>
+                <div className="details">
+                  <div className="buy">
+                    <span>{product.price} &euro;</span>
+                    <Link to={`/products/${product._id}`} state={product}>
+                      BUY NOW
+                    </Link>
+                    {/* <button onClick={() => handleAddProductToCart(product)}>
+                      BUY NOW
+                    </button>
+                    <div className={`msg ${showMsg ? "msg-show" : ""}`}>
+                      Added to cart
+                    </div> */}
+                    {/* <Link to={`/products/${product._id}`} state={product}>
+                BUY NOW
+              </Link> */}
+                  </div>
+                </div>
+              </div>{" "}
+              <div className="icons">
+                <div className="edit">🖊</div>
+                {/* <FaTrashAlt
+                  className="delete"
+                  role="button"
+                  onClick={() => handleDelete(product._id)}
+                /> */}
+                <div
+                  className="delete"
+                  onClick={() => handleDelete(product._id)}
+                >
+                  🗑
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
--- ts-store/src/components/admin/Products.tsx
+++ ts-store/src/components/admin/Products.tsx
@@ -5,6 +5,7 @@ import { deleteProductApi, getProductsApi } from "../../helpers/ApiCalls";
 import { IProduct } from "../../types/product.types";
 import { FaTrashAlt } from "react-icons/fa";
 import "./Admin.scss";
+import { ProductList } from "./ProductList";
 
 export const Products = () => {
   const { user, products, setProducts, errors, setErrors } = useDataContext();
@@ -12,7 +13,6 @@ export const Products = () => {
   const [showMsg, setShowMsg] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
-  
 
   // fetch data from backend on LOAD
   // and afterwards store them in context
@@ -41,24 +41,6 @@ export const Products = () => {
       product.name?.toLowerCase().includes(search.toLowerCase()) ||
       product.description?.toLowerCase().includes(search.toLowerCase())
   );
-
-  //! edit
-
-  //! delete
-  const handleDelete = async (productId: string) => {
-    if (!user) return;
-
-    // 1.step => delete product at API
-    const response = await deleteProductApi(user.token, productId);
-
-    // 2.step => delete product in state
-    const productsCopy = products.filter(
-      (product) => product._id !== productId
-    );
-    console.log(productsCopy);
-
-    setProducts(productsCopy);
-  };
 
   //!message "Added to cart"
   const handleAddProductToCart = (product: string) => {
@@ -98,53 +80,7 @@ export const Products = () => {
           />{" "}
         </form>
       </div>
-
-      {user && (
-        <div className="content">
-          {([...filteredProducts] || []).reverse().map((product) => (
-            <div key={product._id} className="product">
-              <div>
-                <img src={product.image} alt={product.name} />
-              </div>
-              <div className="data">
-                <div className="name">{product.name}</div>
-                <div>{product.description}</div>
-                <div className="details">
-                  <div className="buy">
-                    <span>{product.price} &euro;</span>
-                    <Link to={`/products/${product._id}`} state={product}>
-                      BUY NOW
-                    </Link>
-                    {/* <button onClick={() => handleAddProductToCart(product)}>
-                      BUY NOW
-                    </button>
-                    <div className={`msg ${showMsg ? "msg-show" : ""}`}>
-                      Added to cart
-                    </div> */}
-                    {/* <Link to={`/products/${product._id}`} state={product}>
-                BUY NOW
-              </Link> */}
-                  </div>
-                </div>
-              </div>{" "}
-              <div className="icons">
-                <div className="edit">🖊</div>
-                {/* <FaTrashAlt
-                  className="delete"
-                  role="button"
-                  onClick={() => handleDelete(product._id)}
-                /> */}
-                <div
-                  className="delete"
-                  onClick={() => handleDelete(product._id)}
-                >
-                  🗑
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
+      <ProductList products={filteredProducts} />
     </div>
   );
 };
