3569f66ea465ff010ec79a5fe89e3ebdbdabb78e
elis-11 <eliza.arzanukaeva@gmail.com>
2022-09-22 21:21:25 +0200

    Edit Product

--- ts-store/src/components/product/ProductDetails.tsx
+++ ts-store/src/components/product/ProductDetails.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
+import { MdCancel, MdSaveAlt } from "react-icons/md";
 import { useParams } from "react-router-dom";
 import { useDataContext } from "../../context/DataProvider";
-import { getProductApi } from "../../helpers/ApiCalls";
-import { IProduct } from "../../types/product.types";
+import { getProductApi, updateProductApi } from "../../helpers/ApiCalls";
+import { IProduct, IProductUpdate } from "../../types/product.types";
 import { ImagePicker } from "./ImagePicker";
 
 export const ProductDetails = () => {
@@ -15,6 +16,7 @@ export const ProductDetails = () => {
 
   const refProductName = useRef<HTMLInputElement>(null);
   const refProductDescription = useRef<HTMLInputElement>(null);
+  // const refProductDescription = useRef<HTMLTextAreaElement>(null);
   const refProductPrice = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -27,6 +29,46 @@ export const ProductDetails = () => {
     };
     fetchProductData();
   }, []);
+
+  //! update product
+  const onProductUpdate = async () => {
+    // if neither name, description or price updated => return
+    if (
+      !user ||
+      !product ||
+      !refProductName.current ||
+      !refProductDescription.current ||
+      !refProductPrice.current
+    )
+      return;
+
+    // update object for sending to API
+    const productUpdate: IProductUpdate = {
+      name: refProductName.current.value,
+      description: refProductDescription.current.value,
+      price: refProductPrice.current.value,
+    };
+
+    // image update => if user picked a new image => place it inside update
+    if (image) productUpdate.image = image;
+
+    // send update data to API (backend)
+    const productUpdated = await updateProductApi(
+      user.token,
+      product._id,
+      productUpdate
+    );
+    console.log(productUpdated);
+
+    // update product in list of products using map
+    const productsUpdated = products.map((_product) => {
+      return _product._id === product._id ? productUpdated : _product;
+    });
+    setProducts(productsUpdated);
+    setProduct(productUpdated);
+    setEditMode(false);
+  };
+  if (!product) return <div>Products loading...</div>;
 
   return (
     <div className="Details">
@@ -41,22 +83,47 @@ export const ProductDetails = () => {
             />
           )}
         </div>
-        <div className="item">
-          <div className="name">{product?.name}</div>
-          <div className="description">{product?.description}</div>
-          <div className="price">{product?.price} 💲</div>
-          <div className="create">
-            <span className="avatar" >
-            <img src={product?.author?.avatar} />
-            </span>
-            <span className="author">
-              {product?.author?.name} 
-              </span>
-              <span className="date">
-              {product?.createdAt?.slice(0, 10)}
-            </span>
-            <div className="edit">🖊</div>
+
+        {editMode ? (
+          // show edit files
+          <div className="product-edit">
+            <input
+              defaultValue={product.name}
+              type="text"
+              ref={refProductName}
+            />
+            <input
+              defaultValue={product.description}
+              type="text"
+              ref={refProductDescription}
+            />
+            {/* <textarea defaultValue={product.description} type="text" ref={refProductDescription}/> */}
+            <input
+              defaultValue={product.price}
+              type="text"
+              ref={refProductPrice}
+            />
+            <div className="edit-icons">
+              <MdSaveAlt className="save" onClick={onProductUpdate} />
+              <MdCancel className="cancel" onClick={() => setEditMode(false)} />
+            </div>
           </div>
+        ) : (
+          <div className="item">
+            <div className="name">{product?.name}</div>
+            <div className="description">{product?.description}</div>
+            <div className="price">{product?.price} 💲</div>
+            <div className="create">
+              <span className="avatar">
+                <img src={product?.author?.avatar} />
+              </span>
+              <span className="author">{product?.author?.name}</span>
+              <span className="date">{product?.createdAt?.slice(0, 10)}</span>
+            </div>
+          </div>
+        )}
+        <div className="edit" onClick={() => setEditMode(!editMode)} style={{cursor: 'pointer'}}>
+          🖊
         </div>
       </div>
     </div>
--- ts-store/src/context/DataProvider.tsx
+++ ts-store/src/context/DataProvider.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-import { loadUserInLocalStorage } from '../helpers/LocalStorage';
+import { loadUserInLocalStorage } from "../helpers/LocalStorage";
 import { IProduct } from "../types/product.types";
 import { IContextData, IUser } from "../types/user.types";
 
@@ -9,7 +9,7 @@ export const useDataContext = () => useContext(DataContext);
 
 type Props = {
   children: ReactNode;
-}
+};
 
 export const DataProvider = ({ children }: Props) => {
   const userLs = loadUserInLocalStorage();
@@ -18,6 +18,55 @@ export const DataProvider = ({ children }: Props) => {
   const [users, setUsers] = useState<IUser[]>([]);
   const [errors, setErrors] = useState<string>("");
   const [products, setProducts] = useState<IProduct[]>([]);
+
+//   // cart will holt an array of products
+//   const [cart, setCart] = useState<IProduct[]>([]);
+
+//   // calculate totals
+//   const cartTotal = () => {
+//     let sum = cart.reduce((sum, item) => {
+//       return sum + item.quantity * item.price;
+//     }, 0);
+//     return round(sum);
+//   };
+
+//   const updateQuantity = (productId: string, quantityNew: string) => {
+//     let cartUpdated = cart.map((product) => {
+//       if (product._id === productId) {
+//         product.quantity = quantityNew;
+//       }
+//       return product;
+//     });
+//     setCart(cartUpdated);
+//   };
+
+//   const deleteItem= (productId: string)=>{
+//     let cartUpdated=cart.filter(product =>{
+//       product._id !== productId)
+//       setCart(cartUpdated);
+//     }
+//   }
+// // add product to the cart state array
+//   const addProductToCart = (product: IProduct) => {
+//     let cartItems = [...cart];
+//     console.log(productItems);
+
+//     // check if item already in cart
+//     let cartItemFound=cartItems.find(item => item.id === product._id);
+
+//     // if found => increase quantity by one
+//     if(cartItemFound) {
+//       cartItemFound.quantity++;
+//       setCart(cartItems)
+//     }
+
+//     // if not found - add item with quantity 1
+//     else {
+//       let cartItemNew={...product, quantity:1}
+//       cartItems.push(cartItemNew);
+//       setCart(cartItems)
+//     }
+//   };
 
   const sharedData = {
     user,
@@ -28,6 +77,12 @@ export const DataProvider = ({ children }: Props) => {
     setErrors,
     products,
     setProducts,
+    // cart,
+    // setCart,
+    // addProductToCart,
+    // updateQuantity,
+    // deleteItem,
+    // cartTotal,
   };
 
   return (
